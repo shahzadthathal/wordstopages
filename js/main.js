17292b9239ac545c55411cb3f8081a5aa8b5defe
Muhammad Shahzad <shahzadthathal@gmail.com>
2015-10-21 03:05:40 +0500

    major design updates and core changes

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('webApp', [
+var app = angular.module('wordsToPages', [
   'ngRoute'
 ]);
 
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider) {
-  
+  app.config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
   
   $routeProvider
-     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-    // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+     .when("/", {templateUrl: "partials/home.html", controller: "MainCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 	
-	
-}]);
+});
 
 /**
- * Controls the Blog
+ * Controls all other Pages
  */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  //console.log("Blog Controller reporting for duty.");
+app.controller('MainCtrl', function ( $scope /*, $location, $http */) {
+  
+});
+
+app.controller('PageCtrl', function ( $scope /*, $location, $http */) {
+  
 });
 
 app.directive('showtab',
@@ -39,31 +40,4 @@ app.directive('showtab',
                 });
             }
         };
-    });
-
-
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function ( $scope /*, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
-	//$scope.pages = function(val) {
-     //alert(val);
-	 
-	/*var words = document.calculator.words.value.replace(/[^0-9]/g,'');
-    var font = document.calculator.font.value;
-    var size = document.calculator.size.value;
-	var spacing = document.calculator.spacing.value;
-    
-    var pages = (1/size)*(font*1)*words*spacing/450;
-	pages = Math.round(pages*10)/10; 
-    if (resultbox) { resultbox.innerHTML = pages + '<span style="font-size:70%;">&nbsp;Pages</span>'; }
-*/	
-
-	//}
-
-
-  
 });
