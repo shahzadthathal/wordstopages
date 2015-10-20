6b3371f564e4d72d74c31c1c6e79979e997161c1
Muhammad Shahzad <shahzadthathal@gmail.com>
2015-10-21 01:28:58 +0500

    fixed tab issue

--- js/main.js
+++ js/main.js
@@ -10,18 +10,10 @@ var app = angular.module('webApp', [
  */
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    // Home
-     //.when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-	 .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
@@ -30,8 +22,22 @@ app.config(['$routeProvider', function ($routeProvider) {
  * Controls the Blog
  */
 app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
+  //console.log("Blog Controller reporting for duty.");
 });
+
+app.directive('showtab',
+    function () {
+        return {
+            link: function (scope, element, attrs) {
+                element.click(function(e) {
+                    e.preventDefault();
+                    $(element).tab('show');
+                });
+            }
+        };
+    });
+
+
 
 /**
  * Controls all other Pages
@@ -55,14 +61,5 @@ app.controller('PageCtrl', function ( $scope /*, $location, $http */) {
 	//}
 
 
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
   
 });
